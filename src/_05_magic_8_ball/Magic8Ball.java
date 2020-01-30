e02a8037748fac0d4b5e6abf774e40ee0277ef8d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-01-30 13:26:58 -0800

    doing

--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -9,11 +9,13 @@ import java.util.Random;
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+public static void main(String[] args) {
 
+	Random randomMaker = new Random();
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+	int randomNumber = randomMaker.nextInt();
 	// 3. Print out this variable
-
+System.out.println(randomNumber);
 	// 4. Get the user to enter a question for the 8 ball
 
 	// 5. If the random number is 0
@@ -32,4 +34,4 @@ public class Magic8Ball {
 
 	// -- write your own answer
 
-}
+}}
