1c7aee22c096ea0d3266969a68cd330aa72e4eb7
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-02-20 13:26:45 -0800

    lotterynumber

--- src/_06_lottery_numbers/lottery_numbers.java
+++ src/_06_lottery_numbers/lottery_numbers.java
@@ -2,9 +2,14 @@ package _06_lottery_numbers;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class lottery_numbers {
 public static void main(String[] args) {
-Random randomMaker = new Random(4);
+	Random gen = new Random(); 
+JOptionPane.showMessageDialog(null, "roll for lottery");
+int randomNumber = gen.nextInt(10);
+int randomNumbe = gen.nextInt()
+}
+}
 
-}
-}
